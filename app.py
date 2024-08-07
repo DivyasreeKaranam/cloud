5253a916dfcaeeef321027c1f88558d4964a4124
DivyasreeKaranam <119149642+DivyasreeKaranam@users.noreply.github.com>
2024-08-07 10:24:32 +0530

    app.py

--- app.py
+++ app.py
@@ -118,4 +118,6 @@ def show_attendance():
 
 
 
-app.run(debug=True)  # Set to False in production
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
